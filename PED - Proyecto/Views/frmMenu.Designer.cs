e9e9014e54fd2196b1dd259131886da3736630eb
Aramis1999 <aramis.iraheta.1999@gmail.com>
2020-03-17 23:59:27 -0600

    Menu

---
+++ PED - Proyecto/Views/frmMenu.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace PED___Proyecto
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
+            this.lblSalir = new System.Windows.Forms.Label();
+            this.lblOpciones = new System.Windows.Forms.Label();
+            this.lblCreditos = new System.Windows.Forms.Label();
+            this.lblFoodash = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSalir
+            // 
+            this.lblSalir.BackColor = System.Drawing.Color.Transparent;
+            this.lblSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblSalir.Font = new System.Drawing.Font("Fall is Coming", 60F);
+            this.lblSalir.ForeColor = System.Drawing.Color.White;
+            this.lblSalir.Location = new System.Drawing.Point(899, 9);
+            this.lblSalir.Margin = new System.Windows.Forms.Padding(0);
+            this.lblSalir.Name = "lblSalir";
+            this.lblSalir.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblSalir.Size = new System.Drawing.Size(172, 103);
+            this.lblSalir.TabIndex = 1;
+            this.lblSalir.Text = "Salir";
+            this.lblSalir.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblSalir.Click += new System.EventHandler(this.lblSalir_Click);
+            this.lblSalir.MouseEnter += new System.EventHandler(this.lblSalir_MouseEnter);
+            this.lblSalir.MouseLeave += new System.EventHandler(this.lblSalir_MouseLeave);
+            // 
+            // lblOpciones
+            // 
+            this.lblOpciones.BackColor = System.Drawing.Color.Transparent;
+            this.lblOpciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblOpciones.Font = new System.Drawing.Font("Fall is Coming", 60F);
+            this.lblOpciones.ForeColor = System.Drawing.Color.White;
+            this.lblOpciones.Location = new System.Drawing.Point(790, 348);
+            this.lblOpciones.Margin = new System.Windows.Forms.Padding(0);
+            this.lblOpciones.Name = "lblOpciones";
+            this.lblOpciones.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblOpciones.Size = new System.Drawing.Size(279, 113);
+            this.lblOpciones.TabIndex = 2;
+            this.lblOpciones.Text = "Opciones";
+            this.lblOpciones.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblOpciones.MouseEnter += new System.EventHandler(this.label1_MouseEnter);
+            this.lblOpciones.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            // 
+            // lblCreditos
+            // 
+            this.lblCreditos.BackColor = System.Drawing.Color.Transparent;
+            this.lblCreditos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblCreditos.Font = new System.Drawing.Font("Fall is Coming", 60F);
+            this.lblCreditos.ForeColor = System.Drawing.Color.White;
+            this.lblCreditos.Location = new System.Drawing.Point(9, 152);
+            this.lblCreditos.Margin = new System.Windows.Forms.Padding(0);
+            this.lblCreditos.Name = "lblCreditos";
+            this.lblCreditos.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblCreditos.Size = new System.Drawing.Size(260, 113);
+            this.lblCreditos.TabIndex = 3;
+            this.lblCreditos.Text = "Creditos";
+            this.lblCreditos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCreditos.MouseEnter += new System.EventHandler(this.lblCreditos_MouseEnter);
+            this.lblCreditos.MouseLeave += new System.EventHandler(this.lblCreditos_MouseLeave);
+            // 
+            // lblFoodash
+            // 
+            this.lblFoodash.BackColor = System.Drawing.Color.Transparent;
+            this.lblFoodash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblFoodash.Font = new System.Drawing.Font("Fall is Coming", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFoodash.ForeColor = System.Drawing.Color.White;
+            this.lblFoodash.Location = new System.Drawing.Point(412, 93);
+            this.lblFoodash.Margin = new System.Windows.Forms.Padding(0);
+            this.lblFoodash.Name = "lblFoodash";
+            this.lblFoodash.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblFoodash.Size = new System.Drawing.Size(260, 416);
+            this.lblFoodash.TabIndex = 4;
+            this.lblFoodash.Text = "\r\nFood\r\nDash\r\n";
+            this.lblFoodash.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblFoodash.MouseEnter += new System.EventHandler(this.lblFoodash_MouseEnter);
+            this.lblFoodash.MouseLeave += new System.EventHandler(this.lblFoodash_MouseLeave);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(478, 602);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(292, 56);
+            this.axWindowsMediaPlayer1.TabIndex = 5;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PED___Proyecto.Properties.Resources.Menu_2_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1080, 720);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.lblFoodash);
+            this.Controls.Add(this.lblCreditos);
+            this.Controls.Add(this.lblOpciones);
+            this.Controls.Add(this.lblSalir);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMenu";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseHover += new System.EventHandler(this.frmMenu_MouseHover);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblSalir;
+        private System.Windows.Forms.Label lblOpciones;
+        private System.Windows.Forms.Label lblCreditos;
+        private System.Windows.Forms.Label lblFoodash;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ PED - Proyecto/Views/frmMenu.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PED___Proyecto
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = @"menu.mp3";
+            wplayer.controls.play();
+        }
+
+        private void panel1_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlSalir_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmMenu_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lblSalir_MouseLeave(object sender, EventArgs e)
+        {
+            lblSalir.ForeColor = Color.White;
+            lblSalir.BorderStyle = BorderStyle.None;
+        }
+
+        private void lblSalir_MouseEnter(object sender, EventArgs e)
+        {
+            lblSalir.ForeColor = Color.FromArgb(255, 224, 192);
+            lblSalir.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            lblOpciones.ForeColor = Color.FromArgb(255, 224, 192);
+            lblOpciones.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            lblOpciones.ForeColor = Color.White;
+            lblOpciones.BorderStyle = BorderStyle.None;
+        }
+
+        private void lblCreditos_MouseLeave(object sender, EventArgs e)
+        {
+            lblCreditos.ForeColor = Color.White;
+            lblCreditos.BorderStyle = BorderStyle.None;
+        }
+
+        private void lblCreditos_MouseEnter(object sender, EventArgs e)
+        {
+            lblCreditos.ForeColor = Color.FromArgb(255, 224, 192);
+            lblCreditos.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void lblFoodash_MouseEnter(object sender, EventArgs e)
+        {
+            lblFoodash.ForeColor = Color.FromArgb(255, 224, 192);
+            lblFoodash.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void lblFoodash_MouseLeave(object sender, EventArgs e)
+        {
+            lblFoodash.ForeColor = Color.White;
+            lblFoodash.BorderStyle = BorderStyle.None;
+        }
+    }
+}
